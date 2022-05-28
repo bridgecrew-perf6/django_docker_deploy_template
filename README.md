b6fb8526e6ef5b62bd75c87774305cdac1156450
onticinc <onticinc@gmail.com>
2022-05-28 15:51:24 -0600

    settings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # django_docker_deploy_template
+
+#1 docker-compose build
+
+#2 docker-compose run --rm app sh -c "django-admin startproject app ."
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j#b@rhgot*c5!k@h9$5d@@-qgj-g4k-geg(!a4z-vnrmwn(g56'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [ ]
 
 # Application definition
 
